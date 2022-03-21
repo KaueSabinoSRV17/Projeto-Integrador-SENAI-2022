b29a7373d1bd8a122af0f47d5ffd96ffd5a94a3b
KaueSabinoSRV17 <81828528+KaueSabinoSRV17@users.noreply.github.com>
2022-03-21 13:04:41 -0300

    Adicionando arquivos
     sem a interface de comando. tive problemas em aprender os comandos e as configurações necessárias para que tudo funcione a partir de prompts de comando

---
+++ _javascript/modal.js
@@ -0,0 +1,10 @@
+function abrirModal() {
+    document.getElementById('modal').style.top = "0 ";
+    document.getElementById('modal').style.display = "block ";
+    document.getElementById('x').style.display = "block ";
+}
+
+function fecharModal() {
+    document.getElementById('modal').style.top = "-100% ";
+    document.getElementById('x').style.display = "none ";
+}
